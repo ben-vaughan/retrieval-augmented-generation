1fd28274ab785c73461a1e67e12f0a9c12543cdd
Ben Vaughan <56156727+ben-vaughan@users.noreply.github.com>
2026-08-31 11:14:38 +0100

    feat(menu): open/close menu, present options

--- infra/adapters/into/Terminal.py
+++ infra/adapters/into/Terminal.py
@@ -1,21 +1,16 @@
 from pathlib import Path
 
-from src.ports.into.UploadDocument import UploadDocumentPort
-
 
 class Terminal:
     def __init__(
         self,
-        upload_use_case: UploadDocumentPort,
-        query_use_case: QueryDocumentPort,
     ):
-        self._upload_use_case = upload_use_case
-        self._query_use_case = query_use_case
+        pass
 
     def run(self) -> None:
         while True:
             self._display_menu()
-            choice = input("\nSelect an option (1-5): ").strip()
+            choice = input("\nSelect an option (1-3): ").strip()
 
             match choice:
                 case "1":
@@ -41,8 +36,7 @@ class Terminal:
             return
 
         try:
-            self._upload_use_case(file_path)
-            print("Document processed successfully.")
+            raise NotImplementedError()
         except Exception as e:
             print(f"Upload failed: {e}")
 
@@ -52,7 +46,6 @@ class Terminal:
             return
 
         try:
-            answer = self._query_use_case.execute(question)
-            print(f"\n--- Answer --- \n{answer}")
+            raise NotImplementedError()
         except Exception as e:
             print(f"Query failed: {e}")
